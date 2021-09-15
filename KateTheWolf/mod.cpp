949bc894fa56e99617dec8ace8c07eb45ce4f296
MainMemory <sonicmike2@yahoo.com>
2021-09-15 11:01:43 -0500

    Added code for jiggle effect and Explode ability.

--- KateTheWolf/mod.cpp
+++ KateTheWolf/mod.cpp
@@ -1,1 +1,172 @@
 #include "pch.h"
+#include "SA2ModLoader.h"
+#include <cmath>
+
+struct JiggleData
+{
+	JiggleInfo* TorsoJiggle;
+	JiggleInfo* TorsoJiggle2;
+	NJS_VECTOR TorsoJigglePos;
+	NJS_VECTOR TorsoNodePos;
+};
+JiggleData* jiggledata[2] = {};
+
+FunctionPointer(JiggleInfo*, InitJiggle, (NJS_OBJECT*), 0x447580);
+
+// void __usercall(JiggleInfo *a1@<eax>)
+static const void* const ApplyJigglePtr = (void*)0x4479E0;
+static inline void ApplyJiggle(JiggleInfo* a1)
+{
+	__asm
+	{
+		mov eax, [a1]
+		call ApplyJigglePtr
+	}
+}
+
+// void __usercall(JiggleInfo *a1@<esi>)
+static const void* const FreeJigglePtr = (void*)0x447910;
+static inline void FreeJiggle(JiggleInfo* a1)
+{
+	__asm
+	{
+		mov esi, [a1]
+		call FreeJigglePtr
+	}
+}
+
+FunctionPointer(void, sub_71F040, (NJS_OBJECT*), 0x71F040);
+DataPointer(SonicCharObj2*, dword_1A51A9C, 0x1A51A9C);
+DataArray(float, flt_1A51A00, 0x1A51A00, 12);
+void AmyJiggleHook(NJS_OBJECT* obj)
+{
+	sub_71F040(obj);
+	if (obj == CharacterModels[396].Model)
+	{
+		NJS_VECTOR a3{};
+		if (jiggledata[dword_1A51A9C->base.PlayerNum])
+		{
+			njCalcVector(_nj_current_matrix_ptr_, &a3, &a3, 0);
+			njCalcVector(flt_1A51A00, &jiggledata[dword_1A51A9C->base.PlayerNum]->TorsoNodePos, &a3, 0);
+		}
+	}
+}
+
+void AmyDisplayHook(ObjectMaster* obj)
+{
+	NJS_CNK_MODEL* tm1 = nullptr;
+	NJS_CNK_MODEL* tm2 = nullptr;
+	if (obj->Data2.Character->CharID2 == Characters_Amy && jiggledata[obj->Data2.Character->PlayerNum])
+	{
+		tm1 = CharacterModels[397].Model->child->chunkmodel;
+		CharacterModels[397].Model->child->chunkmodel = jiggledata[obj->Data2.Character->PlayerNum]->TorsoJiggle->SourceModelCopy->chunkmodel;
+		tm2 = CharacterModels[397].Model->child->child->chunkmodel;
+		CharacterModels[397].Model->child->child->chunkmodel = jiggledata[obj->Data2.Character->PlayerNum]->TorsoJiggle2->SourceModelCopy->chunkmodel;
+	}
+	Sonic_Display(obj);
+	if (obj->Data2.Character->CharID2 == Characters_Amy && jiggledata[obj->Data2.Character->PlayerNum])
+	{
+		CharacterModels[397].Model->child->chunkmodel = tm1;
+		CharacterModels[397].Model->child->child->chunkmodel = tm2;
+	}
+}
+
+void doMagicHands() {
+	CharObj2Base* v2 = MainCharObj2[0];
+	if (Upgrades_SonicMagicGloves && v2->CharID2 == Characters_Amy && v2->field_D[0] == 0) {
+		v2->field_D[0] = 74;
+	}
+	//PrintDebug("%d", v2->HeldObject);
+}
+
+extern "C"
+{
+	__declspec(dllexport) void OnFrame()
+	{
+		for (int i = 0; i < 2; i++)
+			if (MainCharObj2[i] && MainCharObj2[i]->CharID2 == Characters_Amy)
+			{
+				if (!jiggledata[i])
+				{
+					jiggledata[i] = new JiggleData();
+					jiggledata[i]->TorsoJiggle2 = InitJiggle(CharacterModels[397].Model->child->child);
+					jiggledata[i]->TorsoJiggle2->type = 14;
+					jiggledata[i]->TorsoJiggle2->speed = 0.40000001f;
+					jiggledata[i]->TorsoJiggle2->field_8 = 12288;
+					jiggledata[i]->TorsoJiggle2->field_10 = 1024;
+					jiggledata[i]->TorsoJiggle2->Model = CharacterModels[397].Model->child->child;
+					jiggledata[i]->TorsoJiggle = InitJiggle(CharacterModels[397].Model->child);
+					jiggledata[i]->TorsoJiggle->type = 13;
+					jiggledata[i]->TorsoJiggle->speed = 0.40000001f;
+					jiggledata[i]->TorsoJiggle->field_8 = 12288;
+					jiggledata[i]->TorsoJiggle->field_10 = 1024;
+					jiggledata[i]->TorsoJiggle->Model = CharacterModels[397].Model->child;
+					jiggledata[i]->TorsoJigglePos = MainCharObj1[i]->Position;
+				}
+				float v46 = jiggledata[i]->TorsoNodePos.y;
+				float v47 = jiggledata[i]->TorsoNodePos.z;
+				NJS_VECTOR v115 = { };
+				v115.x = jiggledata[i]->TorsoNodePos.x;
+				float v48 = v115.x - jiggledata[i]->TorsoJigglePos.x;
+				v115.y = v46;
+				v115.z = v47;
+				v115.x = v48;
+				v115.y = v46 - jiggledata[i]->TorsoJigglePos.y;
+				v115.z = v47 - jiggledata[i]->TorsoJigglePos.z;
+				float v113 = njScalor(&v115);
+				float v49 = jiggledata[i]->TorsoNodePos.y;
+				float v45 = v113;
+				float v50 = jiggledata[i]->TorsoNodePos.z;
+				jiggledata[i]->TorsoJigglePos.x = jiggledata[i]->TorsoNodePos.x;
+				JiggleInfo* v44 = jiggledata[i]->TorsoJiggle2;
+				jiggledata[i]->TorsoJigglePos.y = v49;
+				jiggledata[i]->TorsoJigglePos.z = v50;
+				float v51 = fabs(v45);
+				float v52 = v51;
+				float v53 = fabs(v44->speed);
+				if (v53 < v52)
+				{
+					v44->speed = v113;
+				}
+				ApplyJiggle(jiggledata[i]->TorsoJiggle2);
+				v45 = v113;
+				JiggleInfo* v54 = jiggledata[i]->TorsoJiggle;
+				float v55 = fabs(v45);
+				float v56 = v55;
+				float v57 = fabs(v54->speed);
+				if (v57 < v56)
+				{
+					v54->speed = v45;
+				}
+				ApplyJiggle(jiggledata[i]->TorsoJiggle);
+			}
+			else if (jiggledata[i])
+			{
+				if (jiggledata[i]->TorsoJiggle)
+				{
+					FreeJiggle(jiggledata[i]->TorsoJiggle);
+					jiggledata[i]->TorsoJiggle = nullptr;
+				}
+				if (jiggledata[i]->TorsoJiggle2)
+				{
+					FreeJiggle(jiggledata[i]->TorsoJiggle2);
+					jiggledata[i]->TorsoJiggle2 = nullptr;
+				}
+				delete jiggledata[i];
+				jiggledata[i] = nullptr;
+			}
+	}
+
+	__declspec(dllexport) void Init(const char* path, const HelperFunctions& helperFunctions)
+	{
+		WriteData((void (**)(NJS_OBJECT*))0x72018B, AmyJiggleHook);
+		WriteData((void (**)(ObjectMaster*))0x7172E8, AmyDisplayHook);
+		WriteCall((void*)0x79427A, doMagicHands); //Make Magic Hands prompt automatic
+		WriteData<1>((int*)0x79427F, 0xC3u); //return
+		WriteData<19>((void*)0x72420B, 0x90u);
+		WriteData<11>((void*)0x724229, 0x90u);
+		WriteData<3>((void*)0x724238, 0x90u);
+	}
+
+	__declspec(dllexport) ModInfo SA2ModInfo = { ModLoaderVer };
+}
